ec4af4c686c1f941fcb72b1f27c6cc3ec4d1efd8
Mefew <matheus_i@hotmail.com>
2017-04-12 17:46:24 -0300

    Commit final

--- principal.js
+++ principal.js
@@ -11,4 +11,24 @@
 // .quantidade: todos os input[type=number] com a quantidade de dados a serem rolados
 // #quantidadeD{4,6,8,10,12,20}: um ID para cada input[type=number] com a quantidade
 
+document.getElementById('rolar').addEventListener("click", function() {
+    let resultados = document.getElementById('resultado');
+    let dados = [4,6,8,10,12,20];
+    let somaTotal = 0;
+    resultados.innerHTML="";
 
+    for (var i = 0; i < dados.length; i++) {
+        var numeroRolado = Math.ceil(Math.random() * dados[i]);
+        numeroRolado *= document.getElementById("quantidadeD"+dados[i]).value;
+        resultados.innerHTML += numeroRolado;
+        somaTotal += numeroRolado;
+
+        if (i != dados.length - 1)
+            resultados.innerHTML += " + ";
+        else
+            resultados.innerHTML += " = ";
+    }
+    resultados.innerHTML += somaTotal;
+
+    document.getElementById('recipienteResultados').classList.remove('oculto');
+});
